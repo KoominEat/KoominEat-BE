c2fc0b69967cf18abe6dc4d9270e4d720400df2c
taehwan <th2194@naver.com>
2025-11-19 17:04:05 +0900

    initial project setup

---
+++ src/main/java/com/koomineat/koomineat/KoominEatApplication.java
@@ -0,0 +1,13 @@
+package com.koomineat.koomineat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoominEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoominEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koomineat/koomineat/KoominEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koomineat.koomineat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoominEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
