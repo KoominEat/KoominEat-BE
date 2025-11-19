47497fb12a0e53fa7f1940f3167ee902c5cd8ec2
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2025-11-19 23:09:33 +0900

    user 모델 밑 register, login 구현 완료

---
+++ src/main/java/com/koomineat/koomineat/global/domain/auth/entity/User.java
@@ -0,0 +1,23 @@
+package com.koomineat.koomineat.global.domain.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+// postgres와 충돌 피하기 위해.
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // UUID.
+    @Column(unique = true)
+    private String authToken;
+}
---
+++ src/main/java/com/koomineat/koomineat/global/domain/auth/service/UserService.java
@@ -0,0 +1,63 @@
+package com.koomineat.koomineat.global.domain.auth.service;
+
+import com.koomineat.koomineat.global.domain.auth.entity.User;
+import com.koomineat.koomineat.global.domain.auth.repository.UserRepository;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.servlet.http.HttpServletResponse;
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    public static final String COOKIE_NAME = "AUTH_TOKEN";
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public User registerAndIssueCookie(String name, HttpServletResponse response) {
+        // 토큰 랜덤 생성
+        String token = UUID.randomUUID().toString();
+
+        // user 생성.
+        User user = User.builder()
+                .name(name)
+                .authToken(token)
+                .build();
+        user = userRepository.save(user);
+
+
+        // set cookie 배포시에 수정해야 함.
+        ResponseCookie cookie = ResponseCookie.from(COOKIE_NAME, token)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Lax")
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
+        return user;
+    }
+
+    // 그냥 user만 있다면 return true.
+    @Transactional(readOnly = true)
+    public boolean authenticateByCookie(String tokenFromCookie) {
+        if (tokenFromCookie == null || tokenFromCookie.isBlank()) return false;
+        return userRepository.findByAuthToken(tokenFromCookie).isPresent();
+    }
+    
+    // 디버깅용 user get 함수.
+    public User getUserFromAuthToken(String authToken)
+    {
+        return userRepository.findByAuthToken(authToken).orElseThrow(() -> new RuntimeException("Invalid auth token"));
+    }
+
+
+}
