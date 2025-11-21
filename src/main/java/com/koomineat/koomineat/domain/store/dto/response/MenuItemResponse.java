893c42f918dd5e653755f7526a0c079fa1100c87
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2025-11-21 19:57:04 +0900

    menuItem에 image 필드 추가.

--- src/main/java/com/koomineat/koomineat/domain/store/dto/response/MenuItemResponse.java
+++ src/main/java/com/koomineat/koomineat/domain/store/dto/response/MenuItemResponse.java
@@ -11,12 +11,14 @@ public class MenuItemResponse {
     private Long menuId;
     private String name;
     private Integer price;
+    private String image;
 
     public static MenuItemResponse from(MenuItem m) {
         return MenuItemResponse.builder()
                 .menuId(m.getId())
                 .name(m.getName())
                 .price(m.getPrice())
+                .image(m.getImage())
                 .build();
     }
 }
